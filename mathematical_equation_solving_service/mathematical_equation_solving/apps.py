41626ab89468338ba3bdec7c73131b063879d8ea
singw <singwane.linda.m@gmail.com>
2024-12-16 16:35:25 +0200

    Initial commit for the mathematical equation solving service project

---
+++ mathematical_equation_solving_service/mathematical_equation_solving/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MathematicalEquationSolvingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mathematical_equation_solving'
